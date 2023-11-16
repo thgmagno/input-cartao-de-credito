9480104b77f80ef80b540ea4a882a9646c48ac0d
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2023-11-16 19:09:14 -0300

    update components/CardSides/Front.tsx

--- src/components/CardSides/Front.tsx
+++ src/components/CardSides/Front.tsx
@@ -7,7 +7,7 @@ export default function FrontCard() {
     const { card } = useStore()
 
     return (
-        <section className="credit-card flex flex-col justify-between rounded-xl p-2 shadow-lg w-[280px] h-[176px] bg-gradient-to-r from-neutral-600 to-neutral-950">
+        <section className="credit-card flex flex-col justify-between rounded-xl p-2 shadow-lg w-[280px] h-[176px] bg-gradient-to-r from-neutral-600 to-neutral-950" style={{ userSelect: "none" }}>
             {brand}
             <div className="flex mx-6 ring-2 ring-indigo-500 rounded-md">
                 <span defaultValue={card.numbers} className="text-white p-1 w-full text-center">
@@ -15,13 +15,13 @@ export default function FrontCard() {
                 </span>
             </div>
             <div className="grid grid-cols-2 text-xs md:text-sm overflow-hidden text-white">
-                <label htmlFor="cardHolder">
+                <label>
                     <p className="font-bold">Cardholder</p>
                     <span>
                         {card.cardHolder}
                     </span>
                 </label>
-                <label htmlFor="expires" className="text-end">
+                <label className="text-end">
                     <p className="font-bold">Expires</p>
                     <span className="text-end w-full">
                         {card.expiresMonth}/{card.expiresYear}
